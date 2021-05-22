2f67ad4a9a925f1094514dca8385409285ca109b
Borys52 <borystarasov@gmail.com>
2021-05-22 15:04:53 +0300

    commit2105(3)

--- clothesmarket/settings.py
+++ clothesmarket/settings.py
@@ -159,4 +159,4 @@ CART_SESSION_ID='cart'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
